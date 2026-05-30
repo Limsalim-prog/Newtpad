8903ebd948d2a28dbbd135d05b9ea8b728fd9c13
Limsalim-prog <magicman3572@gmail.com>
2026-05-31 05:31:21 +0700

    git ruqiyah biar si qtbuild ga masuk lagi

--- build/Desktop-Debug/.qt/appNewtpad_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appNewtpad")
-set(working_dir "/home/unarch/ProjectsQT/Newtpad")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/unarch/ProjectsQT/Newtpad/build/Desktop-Debug/.qt/appNewtpad_res.txt")
