5c5782ed09a25e6f8faa866a00856482a2726c51
Limsalim-prog <magicman3572@gmail.com>
2026-05-31 05:03:48 +0700

    Rework_v0.0.2

---
+++ build/Desktop-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_appNewtpad_FILE /home/unarch/ProjectsQT/Newtpad/build/Desktop-Debug/appNewtpad)
+set(__QT_DEPLOY_TARGET_appNewtpad_TYPE EXECUTABLE)
---
+++ build/Desktop-Debug/.qt/appNewtpad_qml.cmake
@@ -0,0 +1,13 @@
+
+set(target "appNewtpad")
+set(working_dir "/home/unarch/ProjectsQT/Newtpad")
+set(src_and_dest_list
+    "/home/unarch/ProjectsQT/Newtpad/Main.qml"
+    "/home/unarch/ProjectsQT/Newtpad/build/Desktop-Debug/Newtpad/Main.qml"
+    "/home/unarch/ProjectsQT/Newtpad/NoteCard.qml"
+    "/home/unarch/ProjectsQT/Newtpad/build/Desktop-Debug/Newtpad/NoteCard.qml"
+    "/home/unarch/ProjectsQT/Newtpad/EditNotePage.qml"
+    "/home/unarch/ProjectsQT/Newtpad/build/Desktop-Debug/Newtpad/EditNotePage.qml"
+
+)
+set(timestamp_file "/home/unarch/ProjectsQT/Newtpad/build/Desktop-Debug/.qt/appNewtpad_qml.txt")
