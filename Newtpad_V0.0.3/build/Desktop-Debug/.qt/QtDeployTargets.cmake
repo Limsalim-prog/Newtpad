74bfec206780ef051e8306bfb2a8234877014872
Salim Ar-Rayyan <99531879+Limsalim-prog@users.noreply.github.com>
2026-06-03 15:16:57 +0700

    Delete Newtpad_V0.0.3/build/Desktop-Debug directory
    gak dibutuhin

--- Newtpad_V0.0.3/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appNewtpad_FILE /home/unarch/ProjectsQT/Newtpad/Newtpad_V0.0.3/build/Desktop-Debug/appNewtpad)
-set(__QT_DEPLOY_TARGET_appNewtpad_TYPE EXECUTABLE)
--- Newtpad_V0.0.3/build/Desktop-Debug/.qt/appNewtpad_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appNewtpad")
-set(working_dir "/home/unarch/ProjectsQT/Newtpad/Newtpad_V0.0.3")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/unarch/ProjectsQT/Newtpad/Newtpad_V0.0.3/build/Desktop-Debug/.qt/appNewtpad_res.txt")
